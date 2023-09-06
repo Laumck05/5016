37b801734a842cf79397a4355d7563e0d649137e
Laumck05 <135397074+Laumck05@users.noreply.github.com>
2023-09-07 09:54:56 +1200

    Update #2
    Added in an extra person and also tested the phone number display

--- # Tuple.py
+++ # Tuple.py
@@ -6,7 +6,9 @@
 bank_accounts = (("Joe", 33, "234 Great South Road", "022629107"),
                  ("Tama", 6000),
                  ("Suzanne", 21025, "45 Green Lane"),
-                 ("Anihera", -75))
+                 ("Anihera", -75),
+                 ("Sam", 543, "5 Long Road", "0274634934"))
+                 
  
 print("The number of bank accounts is ", len(bank_accounts))
  
@@ -19,3 +21,7 @@ for i in bank_accounts:
     if len(i)>2:
         print("\nThe name is ", i[0], " and the address is ", i[2])
         
+# showing only names with phone numbers
+for i in bank_accounts:
+    if len(i)>3:
+        print("\nThe name is ", i[0], " and the phone number is ", i[3])        

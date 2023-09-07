f4640df111637981c1f1ce410f06714cf8c3d89b
Laumck05 <135397074+Laumck05@users.noreply.github.com>
2023-09-07 13:46:18 +1200

    Commit

---
+++ # Dictionary.py
@@ -0,0 +1,11 @@
+# Dictionary.py
+#
+# @ author: A. N. Other
+# date: September 2016
+ 
+dictionary_1 = {"V344LDA":2000,
+                "J245DWE":6350,
+                "K265QWS":500}
+ 
+# retrieving a value from the dictionary
+print("The car with registration V344LDA is worth $", dictionary_1["V344LDA"])

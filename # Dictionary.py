064bfccc85ad25097045ef99fd43d9af0d4aeee7
Laumck05 <135397074+Laumck05@users.noreply.github.com>
2023-09-07 19:41:14 +1200

    Commit

--- # Dictionary.py
+++ # Dictionary.py
@@ -1,3 +1,4 @@
+
 # Dictionary.py
 #
 # @ author: A. N. Other
@@ -9,3 +10,14 @@ dictionary_1 = {"V344LDA":2000,
  
 # retrieving a value from the dictionary
 print("The car with registration V344LDA is worth $", dictionary_1["V344LDA"])
+
+# now for a more challenging example.....
+ 
+# create a mapping of state to abbreviation
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
+}

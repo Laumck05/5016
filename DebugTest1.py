fc114885ddc896cc136631a33bb3d57e591ba971
Laumck05 <135397074+Laumck05@users.noreply.github.com>
2023-09-10 19:04:09 +1200

    Testing debug mode. The encryption is working correctly but I have done the decryption incorrectly

---
+++ DebugTest1.py
@@ -0,0 +1,25 @@
+def caesar_cipher(text, key):
+    result = ""
+    for char in text:
+        if char == " ":
+            result += char
+        else:
+            shift = 65 if char.isupper() else 97
+            encrypted_char = chr((ord(char) - shift + key) % 26 + shift)
+            result += encrypted_char
+    return result
+
+def main():
+    user_input = input("Enter the text to encrypt: ")
+    key = int(input("Enter the encryption key (positive for encryption, negative for decryption): "))
+
+    encrypted_text = caesar_cipher(user_input, key)
+
+    if key > 0:
+        print("Encrypted text:", encrypted_text)
+    else:
+        decrypted_text = caesar_cipher(encrypted_text, -key)
+        print("Decrypted text:", decrypted_text)
+
+if __name__ == "__main__":
+    main()

8e532a8d74d263f327973f12445e8ce8872bcf80
Laumck05 <135397074+Laumck05@users.noreply.github.com>
2023-09-11 20:07:08 +1200

    Trying to get the menu to display

--- Assignment2.py
+++ Assignment2.py
@@ -3,3 +3,12 @@ def main():
 
     while True:
         print("Helpdesk Ticketing System")
+        print("Select from the following options:")
+        print("0. Exit")
+        print("1. Submit helpdesk ticket")
+        print("2. Show all tickets")
+        print("3. Respond to ticket by number")
+        print("4. Re-open received ticket")
+        print("5. Display ticket state")
+
+     choice = 

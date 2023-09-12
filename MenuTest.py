02924c397188653c3bb18d60012e15794eb12da3
Laumck05 <135397074+Laumck05@users.noreply.github.com>
2023-09-12 20:30:26 +1200

    Tested in debug - output is working

--- MenuTest.py
+++ MenuTest.py
@@ -1,14 +1,14 @@
 def update_phone_number():
     phone_number = input("Enter your new phone number: ")
-    print("Thank you for updating your phone number to: {update_phone_number}")
+    print(f"Thank you for updating your phone number to: {phone_number}")
 
 def update_email_address():
     email_address = input("Enter your new email address: ")
-    print("Thank you for updating your email address to: {email_address}")
+    print(f"Thank you for updating your email address to: {email_address}")
 
 def update_address():
     address = input("Enter your new address: ")
-    print("Thank you for updating your address to: {address}")
+    print(f"Thank you for updating your address to: {address}")
 
 def request_call_back():
     print("Thank you for requesting a call back. We will be in touch soon")

c0ff49f0fc82c6414f5b619a3e631fc6e29da7a7
Laumck05 <135397074+Laumck05@users.noreply.github.com>
2023-09-14 10:20:34 +1200

    Going through debug

--- Assignment2.py
+++ Assignment2.py
@@ -11,17 +11,48 @@ class Ticket:
         self.status = "Open"
         Ticket.counter += 1
 
+    def provide_response(self, response):
+        self.response = response
+        self.status = "Closed"
+
+    def reopen_ticket(self):
+        self.status = "Reopened"
+
+    def __str__(self):
+        return "Ticket Number: {self.ticket_number}" \
+               "Ticket Creator: {self.ticket_creator_name}" \
+               "Staff ID: {self.staff_id}" \
+               "Email Address: {self.contact_email}" \
+               "Description: {self.description}" \
+               "Response: {self.response}" \
+               "Ticket Status: {self.status}" \
+               
+
 def submit_ticket(tickets):
-        ticket_creator_name = input("Enter your name: ")
-        staff_id = input("Enter your Staff ID: ")
-        contact_email = input("Enter your contact email: ")
-        description = input("Enter a description of the issue: ")
-        ticket = Ticket
-        print(f"Ticket # has been submitted.\n")
+    ticket_creator_name = input("Enter your name: ")
+    staff_id = input("Enter your Staff ID: ")
+    contact_email = input("Enter your contact email: ")
+    description = input("Enter a description of the issue: ")
+    ticket = Ticket(ticket_creator_name, staff_id, contact_email, description)
+    tickets.append(ticket)
+    print(f"Ticket # has been submitted.\n")
 
+def show_all_tickets(tickets):
+    for ticket in tickets:
+        print(ticket)
 
-def main():
-    tickets = []
+def respond_to_ticket(tickets):
+    ticket_number = int(input("Enter the ticket number to respond to: "))
+    for ticket in tickets:
+        if ticket.ticket_number == ticket_number:
+            response = input("Enter your response: ")
+            ticket.provide_response(response)
+            print("Ticket # has been responded to and closed.")
+            return
+        print("Ticket # has not been found")
+
+    def main():
+        tickets = []
 
     while True:
         print("Helpdesk Ticketing System")

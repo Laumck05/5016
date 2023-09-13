819c47f8cfff5f84f172c657b618f70d465524ae
Laumck05 <135397074+Laumck05@users.noreply.github.com>
2023-09-13 20:40:29 +1200

    Debug mode - changes made

--- Assignment2.py
+++ Assignment2.py
@@ -1,3 +1,20 @@
+class Tickets:
+    counter = 1
+
+    def __init__(self, ticket_creator_name, staff_id, contact_email, description):
+        self.ticket_number = Ticket.counter + 2000
+        self.ticket_creater_name = ticket_creator_name
+        self.staff_id = staff_id
+        self.contact_email = contact_email
+        self.description = description
+        self.response = "Not Yet Provided"
+        self.status = "Open"
+        Ticket.counter += 1
+
+    def submit_ticket(tickets):
+        ticket_creator_name = input("Enter your name: ")
+
+
 def main():
     tickets = []
 
@@ -12,6 +29,21 @@ def main():
         print("5. Display ticket state")
 
         choice = input("Enter menu selection 0 - 5")
+
+        if choice == "0":
+            break
+        elif choice == "1":
+            submit_ticket(tickets)
+        elif choice == "2":
+            show_all_tickets(tickets)
+        elif choice == "3":
+            respond_to_ticket(tickets)
+        elif choice == "4":
+            reopen_received_ticket(tickets)
+        elif choice == "5":
+            display_ticket_state(tickets)
+        else:
+            print("Invalid selection. Please choose a valid option.\n")
 
 if __name__ == "__main__":
     main ()
